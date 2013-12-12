5c4b5228cfa63422c34f40f721952453ab24bbd7
Kuree <xmlc001@gmail.com>
2013-12-12 18:26:01 -0500

    Add slider for Bmail checker
    Bmail checker slider.

--- js/options.js
+++ js/options.js
@@ -5,4 +5,14 @@ document.getElementById("mybucknellcount").innerHTML = localStorage["mybucknellc
 document.getElementById("librarycount").innerHTML = localStorage["librarycount"];
 document.getElementById("moodlecount").innerHTML = localStorage["moodlecount"];
 document.getElementById("innetworkcount").innerHTML = localStorage["innetworkcount"];
+
+var sliderVal;
+
+$('#rangeSlider').slider().on('slide', function (ev) {
+            sliderVal = ev.value;
+        });
+        if (sliderVal) {
+            $('#rangeSlider').slider('setValue', sliderVal);
+        }
+
 };
